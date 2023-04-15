381a1037b9755511ed0f9b07b10cf8722605de05
SeffenTeraNinja <66901505+SeffenTeraNinja@users.noreply.github.com>
2023-04-15 20:22:20 +0200

    base project

---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,30 @@
+<div class="iq-sidebar">
+    <div class="iq-sidebar-logo d-flex justify-content-between">
+
+        <div class="iq-menu-bt-sidebar">
+            <div class="iq-menu-bt align-self-center">
+                <div class="wrapper-menu">
+                    <div class="main-circle">
+                        <i class="ri-arrow-left-s-line"></i>
+                    </div>
+                    <div class="hover-circle">
+                        <i class="ri-arrow-right-s-line"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="sidebar-scrollbar">
+        <nav class="iq-sidebar-menu">
+            <ul class="iq-menu">
+                {{--  <li class="{{ active_link('employees') }}">
+                    <a href="{{ url('dashboard/employees') }}" class="iq-waves-effect">
+                        <img src="{{ asset('assets/images/icons/users.svg') }}" alt="users" class="images-sidebar" />
+                        <span> {{ trans('admin.employees') }} </span>
+                    </a>
+                </li>  --}}
+            </ul>
+        </nav>
+        <div class="p-3"></div>
+    </div>
+</div>
